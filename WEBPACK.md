4ee28eb5b7cc59325a9d42ec3e382e05244c0c71
chenaihua <1690133293@qq.com>
2024-05-28 09:02:11 +0800

    创建webpack.md文件

---
+++ WEBPACK.md
@@ -0,0 +1,10 @@
+# webpack5
+
+## 使用watch mode（观察模式）
+
+在每次编译代码时，手动运行npx webpack会显得很麻烦。
+
+我们可以在webpack启动时添加watch参数，如果其中一个文件被更新，代码将被重新编译，所以你不必再去手动运行整个构建。
+
+`npx webpack --watch`
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ document.querySelector("#content").innerHTML = `<div>Webpack</div>`;
 
 // 前端-注入环境变量
 if (process.env.NODE_ENV === "production") {
-  console.log = function () {};
+  console.log = function () { };
 }
-console.log("开发模式下好用，生产模式下失效");
+console.log("开发模式下好用，生产模式下失效。");
